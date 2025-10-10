84363c488b6e272a1749d511f51691ea021a0bdc
kalyani <enupalakalyani123@gmail.com>
2025-10-10 13:34:07 +0530

    set proper status codes using response entity and integrated Lombok

--- src/main/java/com/kalyani/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/kalyani/journalApp/controller/JournalEntryController.java
@@ -6,8 +6,9 @@ import com.kalyani.journalApp.repository.JournalEntryRepo;
 import com.kalyani.journalApp.service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -23,40 +24,56 @@ public class JournalEntryController {
     private JournalEntryRepo journalEntryRepo;
 
     @GetMapping("/abc")
-    public List<JournalEntry> getAll() {
+    public ResponseEntity<?> getAll() {
 
-        return journalEntryService.getAll();
+        List<JournalEntry>all= journalEntryService.getAll();
+        if(all!=null && !all.isEmpty())
+        {
+            return new ResponseEntity<>(all,HttpStatus.OK);
+        }
+        return  new  ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/create")
-    public JournalEntry createEntry(@RequestBody JournalEntry journalEntry) {
-        journalEntry.setDate(LocalDateTime.now());
-        journalEntryService.save(journalEntry);
-        return journalEntry;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry journalEntry) {
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryService.save(journalEntry);
+            return new ResponseEntity<>(journalEntry, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @GetMapping("/id/{myId}")
-    public Optional<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
-        return journalEntryService.findById(myId);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
+
+        Optional<JournalEntry> optional = journalEntryService.findById(myId);
+        if (optional.isPresent()) {
+            return new ResponseEntity<>(optional.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/id/{myId}")
-    public Boolean deleteJournalEntryById(@PathVariable ObjectId myId) {
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId) {
         journalEntryService.delteById(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/id/{id}")
-    public JournalEntry updateJournalEntry(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry) {
-         JournalEntry old=journalEntryService.findById(id).orElse(null);
-         if(old!=null)
-         {
-             old.setTitle(newEntry.getTitle()!=null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
-             old.setContent(newEntry.getContent()!=null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
-         }
-         journalEntryService.save(old);
+    public ResponseEntity<?> updateJournalEntry(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry) {
+        JournalEntry old = journalEntryService.findById(id).orElse(null);
+        if (old != null) {
+            old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
+            journalEntryService.save(old);
+            return new ResponseEntity<>(old, HttpStatus.OK);
+        }
 
-        return old;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 }
 
--- src/main/java/com/kalyani/journalApp/service/JournalEntryService.java
+++ src/main/java/com/kalyani/journalApp/service/JournalEntryService.java
@@ -5,7 +5,6 @@ import com.kalyani.journalApp.repository.JournalEntryRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
