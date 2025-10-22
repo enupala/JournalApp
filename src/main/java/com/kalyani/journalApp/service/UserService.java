daf9cfdd5bcdc095dc7a5fa15476aa925f0a80c3
kalyani <enupalakalyani123@gmail.com>
2025-10-22 15:17:17 +0530

    Integrated logback and slf4j loggers

--- src/main/java/com/kalyani/journalApp/service/UserService.java
+++ src/main/java/com/kalyani/journalApp/service/UserService.java
@@ -2,7 +2,10 @@ package com.kalyani.journalApp.service;
 
 import com.kalyani.journalApp.entity.User;
 import com.kalyani.journalApp.repository.UserRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,9 +16,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+    private static final Logger logger= LoggerFactory.getLogger(UserService.class);
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     public void save(User user) {
@@ -23,9 +28,20 @@ public class UserService {
     }
 
     public void saveNewUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepo.save(user);
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepo.save(user);
+        }
+        catch (Exception e) {
+           // logger.error("Error occurred for {} : ",user.getUserName(),e);
+            log.error("Error occurred for {} : ",user.getUserName(),e);
+            logger.info("Error occurred for {} : ",user.getUserName(),e);
+            logger.warn("Error occurred for {} : ",user.getUserName(),e);
+         //  these both needs customisation
+          logger.debug("Error occurred for {} : ",user.getUserName(),e);
+            logger.trace("Error occurred for {} : ",user.getUserName(),e);
+        }
     }
 
     public void saveAdminUser(User user) {
